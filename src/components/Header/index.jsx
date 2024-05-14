0d912738e5c7908be685f1129bafd39c9aa1074c
gabrielsouzaleite <gabriel.s.leite05@gmail.com>
2024-05-14 10:15:56 -0300

    Projeto em andamento

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,20 +2,25 @@ import { RiShutDownLine } from 'react-icons/ri';
 import { Container, Profile, Logout } from './styles';
 import { useAuth } from '../../hooks/auth';
 
+import { api } from '../../services/api';
+
+
 export function Header() {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
   return (
     <Container>
       <Profile to="/profile">
         <img
-          src="https://github.com/gabrielsouzaleite.png"
-          alt="Foto do usuário"
+          src={avatarUrl}
+          alt={user.name}
         />
 
         <div>
           <span>Bem-vindo</span>
-          <strong>Gabriel Souza Leite</strong>
+          <strong>{user.name}</strong>
         </div>
       </Profile>
 
